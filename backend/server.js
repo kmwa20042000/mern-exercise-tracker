46cb38760fa6c294d26ffce4f220ea414027bbb5
Kazu Onishi <kmwa20042000@gmail.com>
2020-06-24 01:17:21 +0800

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+	useNewUrlParser: true,
+	useCreateIndex: true
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log('MongoDB db connected');
+});
+
+const exercisesRouter = require('./router/exercises');
+const usersRouter = require('./router/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+	console.log(`server running on: ${port}`);
+});
